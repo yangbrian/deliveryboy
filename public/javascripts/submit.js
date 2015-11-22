b4b1dc2e9c4b118307fbd22baa375b3dd5666dbd
Nikhin Kochath <nikhin.kochath1@gmail.com>
2015-11-22 16:29:45 +0000

    Added some basic form validation, added orders to sidebar, need to modularize socket

--- public/javascripts/submit.js
+++ public/javascripts/submit.js
@@ -12,40 +12,60 @@ var user = {
 };
 
 //create user login based on their facebook and save addresses, restaurants, orders etce
+function validate() {
+ //Checks that fields aren't empty and that cost/tip are valid numbers. Need to add checking for valid addresses
+    for (var x in user) {
+        if (user[x] === '') {
+            alert('Please enter a value for your ' + x);
+            return false;
+        } else if(x === 'cost' || x === 'tip'){
+            
+            var num = parseFloat(user[x], 10);
+            var notNum = isNaN(num);
+            
+            if(notNum || num <= 0){
+                alert('Please enter a valid ' + x);
+                return false;
+            }
+        }
+    }
+    return true;
+}
 
 orderButton.onclick = function() {
-
-    
-    console.log("TESTING");
-
+    //Checks that all fields are filled
 
     user.name = document.getElementById('name').value;
     user.address = document.getElementById('autocomplete').value;
     user.restaurant = document.getElementById('autocomplete2').value;
     user.order = document.getElementById('order').value;
     user.cost = document.getElementById('cost').value;
-    var ison =  document.querySelector('input[name="tip"]:checked').value;
-    var checkedTip = document.querySelector('input[name="tip"]:checked').parentNode;
 
-    if (checkedTip.id === 'custom') {
+    var isChecked =document.querySelector('input[name="tip"]:checked');
+    
+    if (isChecked !== null) {
+        var checkedTip = document.querySelector('input[name="tip"]:checked').parentNode;
 
-        user.tip = document.getElementById('customTip').value;
+        if (checkedTip.id === 'custom') {
 
+            user.tip = document.getElementById('customTip').value;
+        }
+        else {
+            user.tip = checkedTip.innerHTML;
+            user.tip = user.tip.slice(user.tip.lastIndexOf(' '), user.tip.length - 1);
 
+        }
     }
-    else {
-        user.tip = checkedTip.innerHTML;
-        user.tip = user.tip.slice(user.tip.lastIndexOf(' '), user.tip.length-1);
 
 
-    }
+  
+   if(validate()){
+       console.log('Thank you for your order!');
+    socket.emit('order',user);   
+   }
 
-    //
-    //socket.emit('order',user);
 
 
-   
+    
+    
 }
-
-
-
