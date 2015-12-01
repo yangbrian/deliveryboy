399820a3fb795e89d78da24774ef3b6c8b3f0694
Brian Yang <brian@brianyang.me>
2015-12-01 12:08:35 -0500

    Merge pull request #4 from JASONews/login

--- app.js
+++ app.js
@@ -12,10 +12,13 @@ var User = require('./models/user');
 
 var ActiveOrder = require('./models/activeOrder');
 
+var Restaurant = require("./models/restaurant");
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var payments = require('./routes/payments');
 var order = require('./routes/order');
+var restaurants = require("./routes/restaurants");
 
 var app = express();
 
@@ -40,6 +43,7 @@ app.use('/', routes);
 app.use('/users', users);
 app.use('/payments', payments);
 app.use('/order', order(app.io));
+app.use('/restaurants', restaurants);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
