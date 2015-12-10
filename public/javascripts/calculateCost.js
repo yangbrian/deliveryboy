36d6769b2b2403b0bfcf8d4d0e07bf59e2bd70b3
Brian Yang <brian@brianyang.me>
2015-12-09 23:35:02 -0500

    Add items from menu to order and calculate cost

--- public/javascripts/calculateCost.js
+++ public/javascripts/calculateCost.js
@@ -1,7 +1,7 @@
 var addressForm = document.getElementById('autocomplete2');
 var menuButton = document.getElementById('menuButton');
-var todaysMenu = '';
-var tableTitle = '<h3>Todays Menu</h3>';
+var todaysMenu;
+var tableTitle = '<h3>Today\'s Menu</h3>';
 var tmp = $.fn.popover.Constructor.prototype.show;
 $.fn.popover.Constructor.prototype.show = function () {
   tmp.call(this);
@@ -10,51 +10,91 @@ $.fn.popover.Constructor.prototype.show = function () {
   }
 };
 
+var orderEntry = $('#order');
+var orderCost = $('#cost');
 
+/**
+ * Load menus and display into popover dialog
+ */
 function loadMenu(){
-todaysMenu = '';
-todaysMenu = '<table class="table table-condensed"><thead><tr><th>Item</th><th>Cost</th></tr></thead><tbody><tr>';
-  if(addressForm.value.length > 0){
-    tableTitle = '<h3>Todays Menu</h3>';
 
-    var address = addressForm.value;
+  var todaysMenu;
+  if (addressForm.value == '') {
+    todaysMenu = $('<p>')
+        .html('Please enter a restaurant name');
+  } else {
+    todaysMenu = $('<table>')
+        .addClass('table table-condensed table-hover popover-menu-table')
+        .append(
+            $('<thead>')
+                .append(
+                    $('<tr>')
+                        .append($('<th>').html('Item'))
+                        .append($('<th>').html('Cost'))
+                )
 
-      $.get('/order/getDishes', function(dishes){
+        );
 
-        for(var i in dishes){
+    $.get('/order/getDishes', function (dishes) {
 
-          if(dishes[i].address === address){
-            todaysMenu += '<td>' +dishes[i].name+ '</td><td>'+dishes[i].price+ '</td>';
-          }
+      var count = 0;
+      $.each(dishes, function (index, dish) {
+        if (dish.address === addressForm.value) {
+          todaysMenu.append($('<tr>')
+              .addClass('menu-popover-row')
+              .append(
+                  $('<td>')
+                      .addClass('menu-item-name')
+                      .html(dish.name)
+              )
+              .append(
+                  $('<td>')
+                      .addClass('menu-item-price')
+                      .html(dish.price)
+              )
+          );
+          count++;
         }
-
-        todaysMenu += '<tr/></tbody></table>';
-
-
       });
 
-  }else{
+      if (count == 0) {
+        todaysMenu = $('<p>')
+            .html('Please enter a restaurant name');
+      }
+    });
 
-    tableTitle = '<h3>Please Enter a Restaurant</h3>';
+    // add order to list of items and add to the total cost
+    $('.menu-popover').on('click', '.menu-popover-row', function () {
 
+      orderEntry.val(
+          (orderEntry.val() == '' ?  '' : (orderEntry.val() + '\n')) +
+          '- ' + $(this).find('.menu-item-name').html()
+      );
+
+      orderCost.val(
+          (Number(orderCost.val()) + Number($(this).find('.menu-item-price').html())).toFixed(2)
+      );
+    });
   }
+
+
+  $('.menu-popover-content').append(todaysMenu);
+  $('#menu-loader').fadeOut();
+
 }
 
 
-  $('#menuButton').popover({
-    //callback: function(){loadMenu();},
-    trigger: 'click focus',
-    title: function(){
-      return tableTitle;
-    },
-    content: function(){
-      return todaysMenu;
-    },
-    html: true
+$('#menuButton').popover({
+  callback: function(){
+      loadMenu();
+  },
+  content: '<div class="spinner" id="menu-loader"><div class="double-bounce1"></div> <div class="double-bounce2"></div></div>',
+  trigger: 'click focus',
+  title: function(){
+    return tableTitle;
+  },
+  html: true,
+  template: '<div class="popover menu-popover" role="tooltip"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content menu-popover-content"></div></div>',
+  placement: 'bottom' // have to put on bottom because menu is inserted dynamically
 
-  });
-
-$('input[name="restaurant"]').on('keyup', function() {
-  console.log("Loading menu");
-  loadMenu();
 });
