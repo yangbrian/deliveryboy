267f59b1d42e8214feecf20e070f035bf5dc1560
nkochath <nikhin.kochath1@gmail.com>
2015-12-11 16:32:27 -0800

    fixed merge conflict

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var autoIncrement = require('mongoose-auto-increment');
 
-
- //var conn = mongoose.connect('mongodb://localhost:27017',{ mongos : true}, function(err) {
+ //
+ // var conn = mongoose.connect('mongodb://localhost:27017',{ mongos : true}, function(err) {
  //  if (!err)
  //    console.log(err);
- //});
+ // });
 
 
 var conn = mongoose.connect('mongodb://10.0.0.12:27017,10.0.0.13:27017,10.0.0.14:27017,10.0.0.40:27017,10.0.0.59:27017,10.0.0.54:27017/deliveryboy',{ mongos : true}, function(err) {
