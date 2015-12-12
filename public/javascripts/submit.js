284fe2f4e72fe3891a2e57a440dfcffe90074434
Brian Yang <brian@brianyang.me>
2015-12-12 00:59:48 -0500

    Create orders with new list of items

--- public/javascripts/submit.js
+++ public/javascripts/submit.js
@@ -2,14 +2,14 @@
 
 
 
-  // braintree.setup(token, "custom", {
-  //   paypal: {
-  //     container: "paypal-container",
-  //   },
-  //   onPaymentMethodReceived: function (obj) {
-  //     doSomethingWithTheNonce(obj.nonce);
-  //   }
-  // });
+// braintree.setup(token, "custom", {
+//   paypal: {
+//     container: "paypal-container",
+//   },
+//   onPaymentMethodReceived: function (obj) {
+//     doSomethingWithTheNonce(obj.nonce);
+//   }
+// });
 
 
 var orderButton = document.getElementById('placeOrder');
@@ -27,35 +27,18 @@ var user = {
 
 //create user login based on their facebook and save addresses, restaurants, orders etce
 function validate() {
- //Checks that fields aren't empty and that cost/tip are valid numbers. Need to add checking for valid addresses
-    for (var x in user) {
-        if (user[x] === '') {
-            alert('Please enter a value for your ' + x);
-            return false;
-        } else if(x === 'cost' || x === 'tip' || x === 'number'){
-
-            var num = parseFloat(user[x], 10);
-            var notNum = isNaN(num);
-
-            if(notNum || num <= 0){
-                alert('Please enter a valid ' + x);
-                return false;
-            }
-        }
-    }
-    return true;
-}
-
-orderButton.onclick = function() {
-    console.log("Order button clicked");
-    //Checks that all fields are filled
-
+    //Checks that fields aren't empty and that cost/tip are valid numbers. Need to add checking for valid addresses
     user.name = document.getElementById('name').value;
     user.number = document.getElementById('phoneNumber').value;
     user.address = document.getElementById('autocomplete').value;
     user.restaurant = document.getElementById('autocomplete2').value;
 
-    //user.order = document.getElementById('order').value;
+    user.order = '';
+
+    $('.new-order-entry').each(function () {
+        user.order += '- ' + $(this).attr('data-item') + '\n';
+    });
+
     user.cost = document.getElementById('total-cost-field').value;
 
     var isChecked =document.querySelector('input[name="tip"]:checked');
@@ -65,7 +48,7 @@ orderButton.onclick = function() {
 
         if (checkedTip.id === 'custom') {
 
-            user.tip = document.getElementById('custom-tip').value;
+            user.tip = document.getElementById('customTip').value;
         }
         else {
             user.tip = checkedTip.innerHTML;
@@ -107,7 +90,7 @@ orderButton.onclick = function() {
                     currency: 'USD'
                 },
                 dataCollector: {
-                paypal: true  // Enables fraud prevention
+                    paypal: true  // Enables fraud prevention
                 },
                 onPaymentMethodReceived: function (obj) {
                     console.log(obj);
@@ -122,30 +105,29 @@ orderButton.onclick = function() {
 function placeOrder() {
     //Checks that all fields are filled
 
-   if(validate()) {
+    if(validate()) {
         console.log("validated");
-       $.post('/order/new', user, function(data) {
-           console.log(data);
-           if (data.error) {
-               var yes = confirm(data.msg);
-               if (yes) {
-                   document.getElementById('spin_fade').style.display='block';
-                   $.post('/order/new_force',user, function(data) {
-                       window.location.href = "/users/home";
-                   });
-               } else {
-                  window.location.href = "/users/home";
-               }
-           } else {
+        $.post('/order/new', user, function(data) {
+            console.log(data);
+            if (data.error) {
+                var yes = confirm(data.msg);
+                if (yes) {
+                    document.getElementById('spin_fade').style.display='block';
+                    $.post('/order/new_force',user, function(data) {
+                        window.location.href = "/users/home";
+                    });
+                } else {
+                    window.location.href = "/users/home";
+                }
+            } else {
                 window.location.href = "/users/home";
-           }
-       });
-   }
+            }
+        });
+    }
 };
 
 var newCost;
 var count = 0;
-
 
 
 
